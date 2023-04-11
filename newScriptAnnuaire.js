e891a5423ea0a83d7bda85a4403955e70b3bebc0
Martin Chanudet <machanudet@stpbb.org>
2023-04-11 16:36:28 +0200

    ajout

--- newScriptAnnuaire.js
+++ newScriptAnnuaire.js
@@ -14,7 +14,7 @@ function verifcontenu(){
       const nomPersonne = document.getElementById("recherche").value;
       const annuaire = document.getElementById("annuaire");
 
-      const groupe = data.data[idGroupe].listePersonne;
+      const groupe = data.data[idGroupe];
       
       // if(groupe){
 
@@ -31,11 +31,45 @@ function verifcontenu(){
         
         const groupeNom = document.createElement("h2");
         groupeNom.textContent = groupe.nom;
-
         annuaire.appendChild(groupeNom);
 
-        console.log(groupe.nom);
-        console.log("Ok !");
+        if(groupe.mail != " " || groupe.telephone != " "){
+          //Récupère le mail et le tel du groupe et l'ajoute à la div
+          const infosGroupe ="Mail: " + groupe.mail + " / Téléphone:" + groupe.telephone;
+          const emailElement = document.createElement("p");
+          emailElement.textContent = infosGroupe;
+          annuaire.appendChild(emailElement);
+        }
+        
+        array.forEach(element => {
+          
+        });
+
+
+        for (let j = 0; j < Object.keys(data.data[idGroupe].listePersonne).length; j++) {
+          const personne = groupe.listePersonne[j];
+          console.log(personne);
+
+          if (personne.nom.toLowerCase().includes(nomPersonne.toLowerCase())) {
+
+            //Récupère et ajoute le Nom à la Div
+            const nomElement = document.createElement("h3");
+            nomElement.textContent = personne.nom;
+            annuaire.appendChild(nomElement);
+
+            //Récupère et ajoute le Mail et le Téléphone de la personne à la div
+            const info = "Mail: " + personne.email + " / Téléphone: " + personne.telephone;
+            const emailElement = document.createElement("p");
+            emailElement.textContent = info;
+            annuaire.appendChild(emailElement);
+
+
+          }
+        }
+
+
+
+
       }else{
         console.log("error");
       }
